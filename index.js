55fabfefd4610de9c30441010ed885b3378db42c
Opeyemi Bello <114588553+iambelloopeyemi@users.noreply.github.com>
2023-03-06 19:14:51 +0100

    stutern SGA 1.4 arrays and functions assignment

---
+++ index.js
@@ -0,0 +1,76 @@
+1; /* What are the differences between mutating array methods and non-mutation array methods in JavaScript.
+List 5 array methods that fall under each of them.
+*/
+
+/* Mutation in context of JavaScript arrays is basically changing the 
+array itself instead of returning a new array with the new changes 
+whereas Non-Mutation is returning a whole new array which has 
+all the changes
+ */
+
+/** Mutating Array
+ * prototype. pop()
+ * prototype. push()
+ * prototype. shift()
+ * prototype. unshift()
+ * prototype. splice()
+ * */
+
+/** Non-mutating Array
+ * prototype. map()
+ * prototype. filter()
+ * prototype. concat()
+ * prototype. slice()
+ * */
+
+2;
+/* Here is an array of languages: [‘C#’, ‘JavaScript’, ‘Ruby’, ‘PHP’, ‘Python’]. Perform this operation on them
+ * Add ‘Kotlin’ to the end of the array
+ * Add ‘Java’ after ‘Ruby’
+ * Remove the first item in the array
+ * Add ’Scala’ and ‘Swift’ to the beginning of the array
+ * Replace ‘PHP’ with ‘Go’ and ‘Rust’
+ */
+
+const programmingLanguages = ["C#", "JavaScript", "Ruby", "PHP", "Python"];
+const programmingLanguagesUpdate1 = programmingLanguages.push("Kotlin");
+const programmingLanguagesUpdate2 = programmingLanguages.splice(3, 0, "Java");
+const programmingLanguagesUpdate3 = programmingLanguages.shift();
+const programmingLanguagesUpdate4 = programmingLanguages.unshift(
+  "Scala",
+  "Swift"
+);
+const programmingLanguagesUpdate5 = programmingLanguages.splice(
+  3,
+  1,
+  "Go",
+  "Rust"
+);
+console.log(programmingLanguages);
+console.log(programmingLanguagesUpdate3);
+
+3; /* What will be the value of fruit after calling the function changeFruit?
+let fruit = ['apple', 'mango', 'banana'];
+	function changeFruit( fruit ) {
+    fruit[2] = "orange";
+    return fruit;
+  }
+*/
+
+let fruit = ["apple", "mango", "banana"];
+function changeFruit(fruit) {
+  fruit[2] = "orange";
+  return fruit;
+}
+
+console.log(changeFruit(fruit)); // [ 'apple', 'mango', 'orange' ]
+
+4; /* Write a function that accepts an array of numbers as an argument. Return the maximum value in the array.
+e.g., max([4, 5, 10, -2]) // maximum value is 10
+*/
+
+5; /* Write a function called valTimesIndex which accepts an array of numbers and returns a new array with each value multiplied by the index it is at in the array:
+e.g.,
+ valTimesIndex([1,2,3]) // [0,2,6]
+ valTimesIndex([5,10,15]) // [0,10,30]
+*/
