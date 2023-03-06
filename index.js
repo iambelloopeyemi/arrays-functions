0b7a14f8fdc35e85559091fc0ac629275c957289
Opeyemi Bello <114588553+iambelloopeyemi@users.noreply.github.com>
2023-03-06 19:38:06 +0100

    stutern SGA 1.4 arrays and functions assignment

--- index.js
+++ index.js
@@ -69,8 +69,25 @@ console.log(changeFruit(fruit)); // [ 'apple', 'mango', 'orange' ]
 e.g., max([4, 5, 10, -2]) // maximum value is 10
 */
 
+function value(a, b, c, d) {
+  let result = Math.max(a, b, c, d);
+  return result;
+}
+
+const maximumValue = value(4, 5, 10, -2);
+console.log(maximumValue);
+
 5; /* Write a function called valTimesIndex which accepts an array of numbers and returns a new array with each value multiplied by the index it is at in the array:
 e.g.,
  valTimesIndex([1,2,3]) // [0,2,6]
  valTimesIndex([5,10,15]) // [0,10,30]
 */
+
+let index = 2;
+
+let valTimesIndex = [1, 2, 3, 4].map(function (num) {
+  let result = num * index;
+  return result;
+});
+
+console.log(valTimesIndex);
